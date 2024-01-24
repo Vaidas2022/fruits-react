cf3d1751a710a58269432e4b3f6e32bf443628b0
Vaidas <mano@mailas.lt>
2024-01-24 09:36:00 +0200

    Added Promise to load data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ComparisonTable from './ComparisonTable';
 import ProductCard from './ProductCard';
 
+const busimiDuomenys = [
+  {
+    name: "Cherry",
+    price: "$1.99",
+    imageUrl: "/images/cherries.png",
+  },
+  {
+    name: "Avocado",
+    price: "$1.99",
+    imageUrl: "/images/avocados.png",
+  },
+  {
+    name: "Bananas",
+    price: "$1.99",
+    imageUrl: "/images/bananas.png",
+  },
+  {
+    name: "Watermelon",
+    price: "$1.99",
+    imageUrl: "/images/watermelon.png",
+  }
+];
+
+
+
+const fetchData = () => {
+  return new Promise(  (resolve, reject) => {
+
+    setTimeout( ()=> {
+      const success = Math.random() > 0.3; //70% tikimybė, kad true 
+      if(success){
+        resolve(busimiDuomenys);
+      } else{
+        reject("Duomenų užkrauti nepavyko");
+      }
+
+    }, 1000 );
+
+  } );
+}
+
+
+
 function App() {
-  const products = [
-    {
-      name: "Cherry",
-      price: "$1.99",
-      imageUrl: "/images/cherries.png",
-    },
-    {
-      name: "Avocado",
-      price: "$1.99",
-      imageUrl: "/images/avocados.png",
-    },
-    {
-      name: "Bananas",
-      price: "$1.99",
-      imageUrl: "/images/bananas.png",
-    },
-    {
-      name: "Watermelon",
-      price: "$1.99",
-      imageUrl: "/images/watermelon.png",
-    }
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect( () => {
+
+    fetchData()
+    .then( d =>   setProducts( d ))
+    .catch( e => console.error(e));
+
+  }, [] );
+  
 
   return (
     <div className="app">
@@ -41,3 +73,10 @@ function App() {
 
 
 export default App;
+
+
+
+
+
+
+
